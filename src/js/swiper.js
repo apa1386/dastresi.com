d8c0f64b6fda680333fbadd058497a8628faab70
Amirparsa Aminyan <a.p.aminyan@gmail.com>
2025-12-23 23:14:12 +0330

    first commit

---
+++ src/js/swiper.js
@@ -0,0 +1,81 @@
+var swiper = new Swiper(".Swiper-one", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
+var swiper = new Swiper(".Swiper-two", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  centeredSlides: false,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+var swiper = new Swiper(".Swiper-three", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: false,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+var swiper = new Swiper(".Swiper-five", {
+  slidesPerView: 6,
+  spaceBetween: 30,
+  centeredSlides: false,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+var swiper = new Swiper(".Swiper-six", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: false,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
